200a79a347974268afdc78146b147b7f103ba072
hanifsyabani <mh070940@gmail.com>
2024-11-28 05:53:49 +0700

    rename notebook

--- API/app.py
+++ API/app.py
@@ -22,8 +22,8 @@ CLASS_NAMES_NUTRIENT = ['Healthy', 'Kalium_Deficiency',
                         'Nitrogen_Deficiency', 'Phosphorus_Deficiency']
 
 
-# plant_model = load_model('model/plants.keras')
-# CLASS_NAMES_PLANTS = ['Bayam', 'Kangkung', 'Selada', 'Timun', 'Tomat', 'bokChoy']
+plant_model = load_model('model/plants_saved')
+CLASS_NAMES_PLANTS = ['Bayam', 'Kangkung', 'Selada', 'Timun', 'Tomat', 'bokChoy']
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -45,11 +45,11 @@ def predict_image_nutrient(image):
     confidence = float(np.max(predictions))
     return CLASS_NAMES_NUTRIENT[pred_index[0]], confidence
 
-# def predict_image_plant(image):
-#     predictions = plant_model.predict(image)
-#     pred_index = np.argmax(predictions, axis=1)
-#     confidence = float(np.max(predictions))
-#     return CLASS_NAMES_PLANTS[pred_index[0]], confidence
+def predict_image_plant(image):
+    predictions = plant_model.predict(image)
+    pred_index = np.argmax(predictions, axis=1)
+    confidence = float(np.max(predictions))
+    return CLASS_NAMES_PLANTS[pred_index[0]], confidence
 
 @app.route('/predictNutrient', methods=['POST'])
 def predict_nutrient():
@@ -99,53 +99,53 @@ def predict_nutrient():
             'error': str(e)
         }), 500
 
-# @app.route('/predictPlant', methods=['POST'])
-# def predict_plant():
-#     try:
-#         if 'plant_img' not in request.files:
-#             return jsonify({"error": "No file part"}), 400
+@app.route('/predictPlant', methods=['POST'])
+def predict_plant():
+    try:
+        if 'plant_img' not in request.files:
+            return jsonify({"error": "No file part"}), 400
         
-#         file = request.files['plant_img']
+        file = request.files['plant_img']
         
-#         if file.filename == '':
-#             return jsonify({"error": "No selected file"}), 400
+        if file.filename == '':
+            return jsonify({"error": "No selected file"}), 400
         
 
-#         if not allowed_file(file.filename):
-#             return jsonify({"error": "File type not allowed"}), 400
+        if not allowed_file(file.filename):
+            return jsonify({"error": "File type not allowed"}), 400
 
-#         # Process image
-#         try:
-#             processed_image = preprocess_image(file)
-#         except Exception as e:
-#             return jsonify({"error": f"Error processing image: {str(e)}"}), 400
+        # Process image
+        try:
+            processed_image = preprocess_image(file)
+        except Exception as e:
+            return jsonify({"error": f"Error processing image: {str(e)}"}), 400
 
-#         # prediction
-#         try:
-#             prediction, confidence = predict_image_plant(processed_image)
-#         except Exception as e:
-#             return jsonify({"error": f"Error making prediction: {str(e)}"}), 500
+        # prediction
+        try:
+            prediction, confidence = predict_image_plant(processed_image)
+        except Exception as e:
+            return jsonify({"error": f"Error making prediction: {str(e)}"}), 500
 
-#         try:
-#             conn = mysql.connect()
-#             cursor = conn.cursor()
-#             cursor.execute("SELECT * from plants WHERE name LIKE %s", (f"%{prediction}%",))
-#             data = cursor.fetchone()
-#         except Exception as e:
-#             return jsonify({"error": f"Error fetch DB: {str(e)}"}), 500
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * from plants WHERE name LIKE %s", (f"%{prediction}%",))
+            data = cursor.fetchone()
+        except Exception as e:
+            return jsonify({"error": f"Error fetch DB: {str(e)}"}), 500
 
-#         return jsonify({       
-#             'predicted_label': prediction,
-#             'confidence': float(confidence),
-#             'data': data,
-#             'status': 'success'
-#         })
+        return jsonify({       
+            'predicted_label': prediction,
+            'confidence': float(confidence),
+            'data': data,
+            'status': 'success'
+        })
 
-#     except Exception as e:
-#         return jsonify({
-#             'status': 'error',
-#             'error': str(e)
-#         }), 500
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'error': str(e)
+        }), 500
 
 
 if __name__ == '__main__':
